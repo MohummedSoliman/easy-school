8e05c1b2df14db8ce75f62e75d1c7d79d05a19e3
Mohamed Ibrahim <mosuliman92@gmail.com>
2024-01-16 21:29:22 +0200

    add lombak annotaion

--- src/main/java/com/mohamed/easyschool/controller/ContactController.java
+++ src/main/java/com/mohamed/easyschool/controller/ContactController.java
@@ -1,4 +1,20 @@
 package com.mohamed.easyschool.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class ContactController {
+
+    @RequestMapping("/contact")
+    public String displayContactPage(){
+        return "contact";
+    }
+
+    @RequestMapping(value = "/saveMsg", method = RequestMethod.POST)
+    public ModelAndView saveMessage(){
+        return new ModelAndView("redirect:/contact");
+    }
 }
--- src/main/java/com/mohamed/easyschool/model/Contact.java
+++ src/main/java/com/mohamed/easyschool/model/Contact.java
@@ -1,11 +1,17 @@
 package com.mohamed.easyschool.model;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@Data
 public class Contact {
 
     private String name;
     private String mobileNum;
     private String email;
     private String subject;
-
+    private String message;
 }
