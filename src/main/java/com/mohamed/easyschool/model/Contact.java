fc25491063dd5f4527c266d7be44e35bda5a6122
Mohamed Ibrahim <mosuliman92@gmail.com>
2024-01-16 21:26:41 +0200

    create contact pojo class

--- src/main/java/com/mohamed/easyschool/model/Contact.java
+++ src/main/java/com/mohamed/easyschool/model/Contact.java
@@ -1,4 +1,11 @@
 package com.mohamed.easyschool.model;
 
+
 public class Contact {
+
+    private String name;
+    private String mobileNum;
+    private String email;
+    private String subject;
+
 }
