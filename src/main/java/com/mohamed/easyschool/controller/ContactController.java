63033ba502f73d81bdc6a607a5d0f95085222b55
Mohamed Ibrahim <mosuliman92@gmail.com>
2024-01-17 11:51:06 +0200

    pass contact object to service create contact method

--- src/main/java/com/mohamed/easyschool/controller/ContactController.java
+++ src/main/java/com/mohamed/easyschool/controller/ContactController.java
@@ -1,5 +1,8 @@
 package com.mohamed.easyschool.controller;
 
+import com.mohamed.easyschool.model.Contact;
+import com.mohamed.easyschool.service.ContactService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,13 +11,21 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class ContactController {
 
+    private final ContactService contactService;
+
+    @Autowired
+    public ContactController(ContactService contactService){
+        this.contactService = contactService;
+    }
+
     @RequestMapping("/contact")
     public String displayContactPage(){
         return "contact";
     }
 
     @RequestMapping(value = "/saveMsg", method = RequestMethod.POST)
-    public ModelAndView saveMessage(){
+    public ModelAndView saveMessage(Contact contact){
+        contactService.saveContactDetails(contact);
         return new ModelAndView("redirect:/contact");
     }
 }
