36354930191782a11e3149a259cee1b1cbaf4cba
Mohamed Ibrahim <mosuliman92@gmail.com>
2024-01-17 11:33:27 +0200

    add boilerplate for adding contact object to DB

--- src/main/java/com/mohamed/easyschool/service/ContactService.java
+++ src/main/java/com/mohamed/easyschool/service/ContactService.java
@@ -1,4 +1,13 @@
 package com.mohamed.easyschool.service;
 
+import com.mohamed.easyschool.model.Contact;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ContactService {
+
+    public boolean saveContactDetails(Contact contact){
+        boolean isSaved = false;
+        return isSaved;
+    }
 }
