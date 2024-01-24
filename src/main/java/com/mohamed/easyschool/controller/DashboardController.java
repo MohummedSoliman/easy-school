b538b7e3ecaca5d91aaa56ae93176fddc4f40040
Mohamed Ibrahim <mosuliman92@gmail.com>
2024-01-24 11:17:47 +0200

    create dashboard controller

--- src/main/java/com/mohamed/easyschool/controller/DashboardController.java
+++ src/main/java/com/mohamed/easyschool/controller/DashboardController.java
@@ -1,4 +1,17 @@
 package com.mohamed.easyschool.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class DashboardController {
+
+    @RequestMapping("/dashboard")
+    public String displayDashboard(Model theModel, Authentication authentication){
+        theModel.addAttribute("username", authentication.getName());
+        theModel.addAttribute("roles", authentication.getAuthorities().toArray());
+        return "dashboard.html";
+    }
 }
